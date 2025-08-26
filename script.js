f39c6672a5035634a282752f3ebefa9af6a066de
Larabueno <lara.elias.bueno@escola.pr.gov.br>
2025-08-26 10:29:04 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,4 +3,16 @@ botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 function mostraPalavrasChave() {
  const texto = document.querySelector('#entrada-de-texto').value;
  const campoResultado = document.querySelector('#resultado-palavrachave');
-campoResultado.textContent = texto;}
+ const palavrasChave = processarTexto(texto);
+campoResultado.textContent = palavrasChave.join(", ");
+}
+function processarTexto(texto){
+    let palavras = texto.split(/\P{L}+/u);
+    let frequencias = [];
+    for(let i in palavras) {
+        frequencias [i] = 0;
+    }
+    console.log(frequencias)
+
+    return palavras;
+}
